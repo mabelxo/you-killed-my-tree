2a370858d192f96425e7c9141762e3e7eac7331f
mabelxo <harpermabel@gmail.com>
2021-03-31 19:01:10 -0400

    player controller, camera follow, sprite imports
    created player controller + camera follow scripts, as well as imported player and tileset sprites; established a test scene

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform followTransform;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player input to trigger movement
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        //actual player movement, physics calculations, etc
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY); // TODO come back to this
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+}
